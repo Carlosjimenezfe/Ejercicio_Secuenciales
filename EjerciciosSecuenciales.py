e66c0971b1cc7a3687e171c578bf64c332abe69f
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 14:09:40 -0500

    Calcular el valor de la masa

--- EjerciciosSecuenciales.py
+++ EjerciciosSecuenciales.py
@@ -4,4 +4,12 @@ Created on Fri Sep  3 13:56:18 2021
 
 @author: Usuario
 """
-
+"""
+1. Haga un algoritmo que calcule la masa de la siguiente fórmula:
+Masa = (presión * volúmen) / (0.37 * (temperatura + 460))
+"""
+presion = float ( input ('Digite el valor de la presion : '))
+volumen = float (input('Digite el valor del volumen :'))
+temperatura = float(input('Digite el valor de la temperatura : '))
+masa = ( presion * volumen ) / ( 0.37 * ( temperatura + 460 ) )
+print(f'El valor de la masa es de : {masa}' )
